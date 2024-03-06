1623b60a767f1b62212c03e7c7c669c4e6458a9b
markjtabad <62597621+markjtabad@users.noreply.github.com>
2024-03-06 20:23:09 +0800

    Added API call for GeoCities

--- QESamples/src/main/java/com/app/api/model/GeoCityModel.java
+++ QESamples/src/main/java/com/app/api/model/GeoCityModel.java
@@ -1,1 +1,19 @@
-Model
+package com.app.api.model;
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries/PH/places',
+  headers: {
+    'X-RapidAPI-Key': '2fed39ffa1mshccbe13965d016b4p1ad7f2jsn871ad9936e72',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
